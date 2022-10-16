7211f5cb8489840d7b44fcbf1796b86314f9b4f2
waiducom <root@wuaidu.com>
2022-10-16 18:42:00 +0800

    Update cloudflare-ufw.sh

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -5,7 +5,7 @@ echo "" >> /tmp/cf_ips
 curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
 
 # Allow all traffic from Cloudflare IPs (no ports restriction)
-for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip comment 'Cloudflare IP'; done
+#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip comment 'Cloudflare IP'; done
 
 ufw reload > /dev/null
 
@@ -17,4 +17,4 @@ ufw reload > /dev/null
 #for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare IP'; done
 
 # Restrict to ports 80 & 443
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
+for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
